c6f68d008a537f249ecd3b3a9beb17c313e8fbe8
LAPTOP-Q59MTK9C\Pokezard <e0538156@u.nus.edu>
2022-09-11 01:49:59 +0800

    bfs for test 1 done

--- BFS.py
+++ BFS.py
@@ -1,29 +1,155 @@
 import sys
 
+
 # Helper functions to aid in your implementation. Can edit/remove
 #############################################################################
 ######## Piece
 #############################################################################
 class Piece:
-    pass
+    def __init__(self, piece_type, x, y):
+        self.piece_type = piece_type
+        self.x = x
+        self.y = y
+
+    def get_coord(self):
+        return chr(self.x + 97), self.y
+
+    def get_numeric_coord(self):
+        return self.y, self.x
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+
+class King(Piece):
+    def __init__(self, x, y):
+        super().__init__('King', x, y)
+
+    def get_actions(self):
+        ls = []
+        right = King(self.get_x() + 1, self.get_y())
+        ls.append(right)
+        left = King(self.get_x() - 1, self.get_y())
+        ls.append(left)
+        down = King(self.get_x(), self.get_y() - 1)
+        ls.append(down)
+        up = King(self.get_x(), self.get_y() + 1)
+        ls.append(up)
+        return ls
+
 
 #############################################################################
 ######## Board
 #############################################################################
 class Board:
-    pass
+    def __init__(self, board, pieces):
+        self.board = board
+
+    def get_board(self):
+        return self.board
+
 
 #############################################################################
 ######## State
 #############################################################################
 class State:
-    pass
+    def __init__(self, board, enemy_pieces, own_piece, goals, max_x, max_y, path, action_cost):
+        self.board = board
+        self.enemy_pieces = enemy_pieces
+        self.own_piece = own_piece
+        self.goals = goals
+        self.max_x = max_x
+        self.max_y = max_y
+        self.path = path
+        self.action_cost = action_cost
+
+    #  multiple potential goals
+    def is_goal(self):
+        for goal in self.goals:
+            #  print([self.own_piece.get_coord(), goal])
+            if self.own_piece.get_numeric_coord() == goal:
+                return True
+        return False
+
+    #  return list of all possible Piece positions to take
+    def get_actions(self):
+
+        # all possible pieces
+        ls = self.own_piece.get_actions()  # regardless of legal
+        ls = list(set(ls))  # remove duplicates
+
+        # remove those pieces outside the board
+        pieces = ls.copy()
+        for piece in ls:
+            # assume a is start
+            if piece.get_x() + 1 > self.max_x or piece.get_x() < 0 or piece.get_y() - 1 < 0 or piece.get_y() + 1 > self.max_y:
+                pieces.remove(piece)
+
+
+        # remove pieces on enemy pieces
+        copy_pieces_enemy = pieces.copy()
+        for enemy_piece in self.enemy_pieces:
+            for own_piece in copy_pieces_enemy:
+                # list comprehension for all possible enemy moves
+                enemy_moves = [i.get_coord() for i in enemy_piece.get_actions()].append(enemy_piece.get_coord())
+                if own_piece.get_coord() in enemy_moves:
+                    pieces.remove(own_piece)
+
+        # remove pieces in obstacles
+        copy_pieces = pieces.copy()
+        for piece in copy_pieces:
+            obstacle = self.board[piece.get_y()][piece.get_x()]
+            is_obstacle = obstacle == -1
+            if is_obstacle:
+                pieces.remove(piece)
+        # print([piece.get_coord() for piece in pieces])
+        return pieces
+
+    #  return all transition states that are legal
+    def get_transition(self):
+        transitions = []
+        for piece in self.get_actions():
+            cost = self.board[piece.get_y()][piece.get_x()]
+            new_path = self.path.copy()
+            new_path.append(piece.get_coord())
+            new_state = State(self.board, self.enemy_pieces, piece, self.goals, self.max_x, self.max_y,
+                              new_path, self.action_cost + cost)
+            transitions.append(new_state)
+        return transitions
+
+    def get_path(self):
+        return self.path
+
+    def get_action_cost(self):
+        return self.action_cost
+
 
 #############################################################################
 ######## Implement Search Algorithm
 #############################################################################
 def search(rows, cols, grid, enemy_pieces, own_pieces, goals):
-    pass
+    start_state = State(grid, enemy_pieces, own_pieces, goals, rows, cols, [own_pieces.get_coord()], 0)
+    queue = []
+    visited = [[False for i in range(cols)] for j in range(rows)]
+    queue.append(start_state)
+    visited[start_state.own_piece.get_y()][start_state.own_piece.get_x()] = True
+    while queue:
+        curr_state = queue.pop(0)
+        if curr_state.is_goal():
+            # print(curr_state.get_action_cost())
+            return curr_state.get_path()
+
+        trans = curr_state.get_transition()
+        for state in trans:
+            piece_x = state.own_piece.get_x()
+            piece_y = state.own_piece.get_y()
+            if not visited[piece_y][piece_x]:
+                visited[piece_y][piece_x] = True
+                queue.append(state)
+    return []
 
 
 #############################################################################
@@ -35,27 +161,28 @@ def parse(testcase):
     handle = open(testcase, "r")
 
     get_par = lambda x: x.split(":")[1]
-    rows = int(get_par(handle.readline())) # Integer
-    cols = int(get_par(handle.readline())) # Integer
-    grid = [[1 for j in range(cols)] for i in range(rows)] # Dictionary, label empty spaces as 1 (Default Step Cost)
-    enemy_pieces = [] # List
-    own_pieces = [] # List
-    goals = [] # List
+    rows = int(get_par(handle.readline()))  # Integer
+    cols = int(get_par(handle.readline()))  # Integer
+    grid = [[1 for j in range(cols)] for i in range(rows)]  # Dictionary, label empty spaces as 1 (Default Step Cost)
+    enemy_pieces = []  # List
+    own_pieces = []  # List
+    goals = []  # List
 
     handle.readline()  # Ignore number of obstacles
     for ch_coord in get_par(handle.readline()).split():  # Init obstacles
         r, c = from_chess_coord(ch_coord)
-        grid[r][c] = -1 # Label Obstacle as -1
+        grid[r][c] = -1  # Label Obstacle as -1
 
     handle.readline()  # Ignore Step Cost header
     line = handle.readline()
     while line.startswith("["):
         line = line[1:-2].split(",")
         r, c = from_chess_coord(line[0])
-        grid[r][c] = int(line[1]) if grid[r][c] == 1 else grid[r][c] #Reinitialize step cost for coordinates with different costs
+        grid[r][c] = int(line[1]) if grid[r][c] == 1 else grid[r][
+            c]  # Reinitialize step cost for coordinates with different costs
         line = handle.readline()
-    
-    line = handle.readline() # Read Enemy Position
+
+    line = handle.readline()  # Read Enemy Position
     while line.startswith("["):
         line = line[1:-2]
         piece = add_piece(line)
@@ -71,16 +198,19 @@ def parse(testcase):
     for ch_coord in get_par(handle.readline()).split():
         r, c = from_chess_coord(ch_coord)
         goals.append((r, c))
-    
+
     return rows, cols, grid, enemy_pieces, own_pieces, goals
 
-def add_piece( comma_seperated) -> Piece:
+
+def add_piece(comma_seperated) -> Piece:
     piece, ch_coord = comma_seperated.split(",")
     r, c = from_chess_coord(ch_coord)
-    return [piece, (r,c)]
+    return [piece, (r, c)]
 
-def from_chess_coord( ch_coord):
-    return (int(ch_coord[1:]), ord(ch_coord[0]) - 97)
+
+def from_chess_coord(ch_coord):
+    return int(ch_coord[1:]), ord(ch_coord[0]) - 97
+
 
 #############################################################################
 ######## Main function to be called
@@ -88,9 +218,17 @@ def from_chess_coord( ch_coord):
 ### DO NOT EDIT/REMOVE THE FUNCTION BELOW###
 # To return: List of moves
 # Return Format Example: [[('a', 0), ('a', 1)], [('a', 1), ('c', 3)], [('c', 3), ('d', 5)]]
-def run_BFS():    
+def run_BFS():
     testcase = sys.argv[1]
     rows, cols, grid, enemy_pieces, own_pieces, goals = parse(testcase)
-    moves = search(rows, cols, grid, enemy_pieces, own_pieces, goals)
+    own_piece = King(own_pieces[0][1][0], own_pieces[0][1][1])
+    ##enemy_pieces_list = []
+    ##for enemy in enemy_pieces:
+    ##    piece = "error"
+    ##    if enemy[0] == "king":
+    ##        piece = King(enemy[1][0], enemy[1][1])
+    ##        enemy_pieces_list.append(piece)
+    moves = search(rows, cols, grid, enemy_pieces, own_piece, goals)
     return moves
-    
+
+print(run_BFS())
